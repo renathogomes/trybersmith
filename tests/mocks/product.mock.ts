e277f230d0285c5435d81aaac0154705fee6ba56
renatho <renatho.20gomes@gmail.com>
2024-01-31 12:40:42 -0300

    testes do req 1

---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,20 @@
+import { Product } from "../../src/types/Product";
+
+const ProductSuccesDB: Product = {
+  id: 1,
+  name: 'Bone Blade',
+  price: '20 peças de diamante',
+  orderId: 4
+};
+
+const ProductSuccesCreated = {
+  id: 1,
+  name: 'Bone Blade',
+  price: '20 peças de diamante',
+  orderId: 4
+};
+
+export default {
+  ProductSuccesDB,
+  ProductSuccesCreated
+}
--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -2,6 +2,12 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import productMock from '../../mocks/product.mock';
+import { ServiceResponse } from '../../../src/types/ServiceResponse';
+import { Product } from '../../../src/types/Product';
+import productService from '../../../src/services/product';
+import productController from '../../../src/controller/product';
+
 
 chai.use(sinonChai);
 
@@ -13,6 +19,21 @@ describe('ProductsController', function () {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
     sinon.restore();
+  });
+
+  it('should verify the controller when successfully creating a new product', async function () {
+    req.body = productMock.ProductSuccesDB;
+
+    const serviceResponse: ServiceResponse<Product> = {
+      status: 'SUCCESSFUL',
+      data: productMock.ProductSuccesCreated,
+    };
+
+    sinon.stub(productService, 'createProduct').resolves(serviceResponse);
+    await productController.createProduct(req, res);
+
+    chai.expect(res.status).to.have.been.calledWith(201);
+    chai.expect(res.json).to.have.been.calledWith(productMock.ProductSuccesCreated);
   });
 
 });
