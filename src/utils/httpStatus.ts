e47bf12f774c85ceba56bee26eab665bf5894dcf
renatho <renatho.20gomes@gmail.com>
2024-01-31 15:41:26 -0300

    criação de util com mapa de status http

---
+++ src/utils/httpStatus.ts
@@ -0,0 +1,13 @@
+export default function statusHTTP(status: string): number {
+  // Mapa de correspondência entre status e códigos HTTP
+  const HTTPmap: Record<string, number> = {
+    INVALID_DATA: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+    SUCCESFUL: 200,
+    CREATED: 201,
+  };
+    
+  // Retorna o código HTTP correspondente ou 500 se não houver correspondência
+  return HTTPmap[status] ?? 500;
+}
