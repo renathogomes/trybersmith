6d919de04ee81f136f4f437072c1f7c9ab651388
renatho <renatho.20gomes@gmail.com>
2024-02-04 14:52:32 -0300

    ajuste no secret, adicionando um ou secret

--- src/utils/tokenJWT.ts
+++ src/utils/tokenJWT.ts
@@ -1,7 +1,7 @@
 // crie usando JWT um token que deve ser gerado e retornado caso haja sucesso no login. No seu payload deve estar presente o id e username, adicione uma tipagem para que id seja number e username seja string
 import jwt from 'jsonwebtoken';
 
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET || 'secret';
 
 type Payload = {
   id: number;
@@ -9,5 +9,5 @@ type Payload = {
 };
 
 export default function tokenJWT(payload: Payload): string {
-  return jwt.sign(payload, secret as string, { expiresIn: '1h' });
+  return jwt.sign(payload, secret);
 }
