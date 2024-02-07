dcc23647283750280155addbccde4bde42e453b6
renatho <renatho.20gomes@gmail.com>
2024-02-07 09:48:53 -0300

    testes req 5

--- tests/unit/controllers/orders.controller.test.ts
+++ tests/unit/controllers/orders.controller.test.ts
@@ -2,6 +2,12 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import orderController from '../../../src/controller/order';
+import orderService from '../../../src/services/order';
+import orderMock from '../../mocks/order.mock';
+import productService from '../../../src/services/product';
+import OrderModel, {OrderSequelizeModel} from '../../../src/database/models/order.model';
+import { ServiceResponse } from '../../../src/types/ServiceResponse';
 
 chai.use(sinonChai);
 
@@ -15,4 +21,16 @@ describe('OrdersController', function () {
     sinon.restore();
   });
 
+  // Deve retornar um array de objetos com os pedidos
+  it('Should return an array of objects with orders', async function () {
+    const ServiceResponse: ServiceResponse<OrderSequelizeModel[]> = {
+      status: 'SUCCESSFUL',
+      data: orderMock.allOrders,
+    };
+
+    sinon.stub(orderService, 'getAll').resolves(ServiceResponse);
+    await orderController.getAll(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+  });
 });
--- tests/unit/services/orders.service.test.ts
+++ tests/unit/services/orders.service.test.ts
@@ -1,7 +1,18 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import OrderModel from '../../../src/database/models/order.model';
+import orderService from '../../../src/services/order';
+import orderMock from '../../mocks/order.mock';
 
 describe('OrdersService', function () {
   beforeEach(function () { sinon.restore(); });
 
+  // Deve retornar um array de objetos com os pedidos
+  it('Should return an array of objects with orders', async function () {
+    sinon.stub(OrderModel, 'findAll').resolves(orderMock.allOrders);
+    const result = await orderService.getAll()
+    
+    expect(result.status).to.be.equal('SUCCESSFUL');
+
+  });
 });
