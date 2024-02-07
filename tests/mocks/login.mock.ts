b187c4776a1acc9e8d7db6538fb571f0ee8f6306
renatho <renatho.20gomes@gmail.com>
2024-02-07 12:18:41 -0300

    fazendo testes

---
+++ tests/mocks/login.mock.ts
@@ -0,0 +1,12 @@
+import { User } from "../../src/types/User";
+
+const LoginUsername = {
+  username: '',
+  password: 'terrível'
+};
+
+export default {
+    LoginUsername,
+};
+
+
--- tests/unit/services/login.service.test.ts
+++ tests/unit/services/login.service.test.ts
@@ -1,7 +1,14 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import loginMock from '../../mocks/login.mock';
+import loginService from '../../../src/services/login';
 
 describe('LoginService', function () {
   beforeEach(function () { sinon.restore(); });
-
+  it('Should verify that if the username is not passed, an error is returned.', async function() {
+    const parameters = loginMock.LoginUsername;
+    const serviceResponse = await loginService(parameters);
+    expect(serviceResponse.status).to.deep.equal('INVALID_DATA');
+    expect(serviceResponse.data).to.deep.equal( { message: '"username" and "password" are required' })
+  });
 });
